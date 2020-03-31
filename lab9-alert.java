67c42c56f3d0a753bb3fc7954853d72c680f55d6
SurakshaRV <43144535+SurakshaRV@users.noreply.github.com>
2020-03-31 13:54:59 +0530

    Create lab9-alert.java

---
+++ lab9-alert.java
@@ -0,0 +1,71 @@
+package com.example.alertapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button notify;
+    EditText e1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notify = (Button)findViewById(R.id.notify);
+        e1 = (EditText)findViewById(R.id.edit1);
+
+        notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = e1.getText().toString();
+                int NOTIFICATION_ID = 234;
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                String CHANNEL_ID = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    CHANNEL_ID = "my_channel_01";
+                    CharSequence name = "my_channel";
+                    String Description = "This is my channel";
+                    int importance = NotificationManager.IMPORTANCE_HIGH;
+                    NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+                    mChannel.setDescription(Description);
+                    mChannel.enableLights(true);
+                    mChannel.setLightColor(Color.RED);
+                    mChannel.enableVibration(true);
+                    mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                    mChannel.setShowBadge(false);
+                    notificationManager.createNotificationChannel(mChannel);
+                }
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("My Notification")
+                        .setContentText(msg);
+
+                Intent resultIntent = new Intent(MainActivity.this, MainActivity.class);
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(MainActivity.this);
+                stackBuilder.addParentStack(MainActivity.class);
+                stackBuilder.addNextIntent(resultIntent);
+                PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(resultPendingIntent);
+                notificationManager.notify(NOTIFICATION_ID, builder.build());
+            }
+        });
+
+    }
+}
