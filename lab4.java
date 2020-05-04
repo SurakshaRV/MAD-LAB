81d488785e0308586a532b70a6bd92ccc37089d3
SurakshaRV <43144535+SurakshaRV@users.noreply.github.com>
2020-05-04 15:32:11 +0530

    Update lab4.java

--- lab4.java
+++ lab4.java
@@ -1,6 +1,6 @@
-package com.example.mad_pgm4graphics;
+package com.example.lab4graphics;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -10,30 +10,56 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Bitmap bg=Bitmap.createBitmap(720,1280,Bitmap.Config.ARGB_8888);
-        ImageView i=(ImageView)findViewById(R.id.imageView);
+        Bitmap bg = Bitmap.createBitmap(720, 1280, Bitmap.Config.ARGB_8888);
+        ImageView i = (ImageView) findViewById(R.id.imageView);
+
         i.setBackgroundDrawable(new BitmapDrawable(bg));
-        Canvas canvas=new Canvas(bg);
-        Paint paint=new Paint();
-        paint.setColor(Color.BLUE);
+        Canvas canvas = new Canvas(bg);
+        Paint paint = new Paint();
+        paint.setColor(Color.CYAN);
         paint.setTextSize(50);
 
-        canvas.drawText("Rectangle",420,150,paint);
-        canvas.drawRect(400,200,650,700,paint);
+        //To draw a Rectangle
+        canvas.drawText("Rectangle", 420, 150, paint);
+        canvas.drawRect(400, 200, 650, 700, paint);
 
-        canvas.drawText("Square",420,150,paint);
-        canvas.drawRect(400,400,650,650,paint);
+        //To draw a Circle
+        canvas.drawText("Circle", 120, 150, paint);
+        canvas.drawCircle(200, 350, 150, paint);
 
-        canvas.drawText("Circle",420,150,paint);
-        canvas.drawCircle(200,200,100,paint);
+        //To draw a Square
+        canvas.drawText("Square", 120, 800, paint);
+        canvas.drawRect(50, 850, 350, 1150, paint);
 
-        canvas.drawText("Rectangle",420,150,paint);
-        canvas.drawLine(0,0,0,50,paint);
+        //To draw a Line
+        canvas.drawText("Line", 480, 800, paint);
+        canvas.drawLine(520, 850, 520, 1150, paint);
     }
 }
+/*
+activity_main.xml
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context=".MainActivity">
+    <ImageView
+
+        android:layout_width="match_parent"
+
+        android:layout_height="match_parent"
+
+        android:id="@+id/imageView"/>
+
+</RelativeLayout>
+
+
+*/
